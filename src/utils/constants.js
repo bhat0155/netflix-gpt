9203c7bfb490c819c950d3c65423d6378cc88185
Ekam Bhatia <ekambhatia@Ekams-MacBook-Pro.local>
2024-11-06 10:26:12 -0500

    deploying

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,13 +2,13 @@ export const NETFLIX_LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const PROFILE_PIC =
   "https://lh3.googleusercontent.com/a/ACg8ocKhHWdLH5J8fXoSvSnp97M9HOVcomhkY9vAFo5lNPrPP6FMgzrD=s576-c-no";
-export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_KEY = "70a99c9926d76490d4a7b25a8a7114e2";
 export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer "+process.env.REACT_APP_TMDB_KEY,
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MGE5OWM5OTI2ZDc2NDkwZDRhN2IyNWE4YTcxMTRlMiIsIm5iZiI6MTczMDM5MzA5NC44MDEwMTczLCJzdWIiOiI2NWY4YjYwNDk1OTBlMzAxODZlZGUyZmUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.EoXjf43dApo6I8MVl8cD-j8cvvRFRQgMf7yraPpvYeQ",
   },
 };
 export const NETFLIX_BACKGROUND =
